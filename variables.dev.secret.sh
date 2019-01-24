908a6fe633e11506a0bd49682f8486343f28333b
Nuno Carvalho <nuno.carvalho@daimler.com>
2019-01-24 10:29:21 +0000

    several small fixes found while testing the current solution; added support for selh hosted nexus repo as the registry service

--- variables.dev.secret.sh
+++ variables.dev.secret.sh
@@ -5,6 +5,10 @@ export ARM_TENANT_ID=""
 export ARM_CLIENT_ID="" #Application ID
 export ARM_CLIENT_SECRET="" # Password
 
+#TODO Uncomment only if using a private self hosted registry service
+#export TF_VAR_NexusUsername="service_account_username"
+#export TF_VAR_NexusPassword="service_account_password"
+
 #Azure Deployment related vars
 export TF_VAR_resourceGroupName="yca027-pg001"
 export TF_VAR_resourceGroupLocation="westeurope"
@@ -18,7 +22,7 @@ export TF_VAR_azureClientSecret=""
 #Azure pool configuration
 export TF_VAR_PoolName="kafkaakstest"
 export TF_VAR_PoolNodeCount=3
-export TF_VAR_PoolVmSize="Standard_D2_v2"
+export TF_VAR_PoolVmSize="Standard_DS2_v2"
 export TF_VAR_PoolDiskSizeGb=30
 
 #agent authentication
